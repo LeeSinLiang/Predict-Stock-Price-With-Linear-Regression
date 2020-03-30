7896a40e89e7e30b42cd2081914fdee3f79d9d8a
James Lee Sin Liang <mangojamlee@gmail.com>
2020-03-30 17:30:43 +0800

    Add files via upload

---
+++ predict_stock_price.py
@@ -0,0 +1,71 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import os
+
+df = pd.read_csv('AAPL.csv')
+dates = list(range(0,int(len(df))))
+prices = df['Close']
+#Impute missing values (NaN)
+prices[np.isnan(prices)] = np.median(prices[~np.isnan(prices)])
+
+#Plot Original Data
+plt.plot(df['Close'], label='Close Price history')
+plt.title('Linear Regression | Time vs. Price (Original Data)')
+plt.legend()
+plt.xlabel('Date Integer')
+plt.show()
+
+#Convert to numpy array and reshape them
+dates = np.asanyarray(dates)
+prices = np.asanyarray(prices)
+dates = np.reshape(dates,(len(dates),1))
+prices = np.reshape(prices, (len(prices), 1))
+
+#Load Pickle File to get the previous saved model accuracy
+try:
+  pickle_in = open("prediction.pickle", "rb")
+  reg = pickle.load(pickle_in)
+  xtrain, xtest, ytrain, ytest = train_test_split(dates, prices, test_size=0.2)
+  best = reg.score(ytrain, ytest)
+except:
+  pass
+
+#Get the highest accuracy model
+best = 0
+for _ in range(100):
+    xtrain, xtest, ytrain, ytest = train_test_split(dates, prices, test_size=0.2)
+    reg = LinearRegression().fit(xtrain, ytrain)
+    acc = reg.score(xtest, ytest)
+    if acc > best:
+        best = acc
+        #Save model to pickle format
+        with open('prediction.pickle','wb') as f:
+            pickle.dump(reg, f)
+        print(acc)
+
+#Load linear regression model
+pickle_in = open("prediction.pickle", "rb")
+reg = pickle.load(pickle_in)
+
+#Get the average accuracy of the model
+mean = 0
+for i in range(10):
+  #Random Split Data
+  msk = np.random.rand(len(df)) < 0.8
+  xtest = dates[~msk]
+  ytest = prices[~msk]
+  mean += reg.score(xtest,ytest)
+
+print("Average Accuracy:", mean/10)
+
+#Plot Predicted VS Actual Data
+plt.plot(xtest, ytest, color='green',linewidth=1, label= 'Actual Price') #plotting the initial datapoints
+plt.plot(xtest, reg.predict(xtest), color='blue', linewidth=3, label = 'Predicted Price') #plotting the line made by linear regression
+plt.title('Linear Regression | Time vs. Price ')
+plt.legend()
+plt.xlabel('Date Integer')
+plt.show()
